0553a5679ac9ce11a0286e958ca97ed44e3fab8b
MelinaTheMad <melinathemad@gmail.com>
2025-01-21 15:10:20 +0100

    Set KEY_OVERRIDE_ENABLE to no

--- keyboards/beekeeb/piantor_pro/keymaps/melina/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/melina/keymap.c
@@ -1,7 +1,6 @@
 // Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
-#include "keycodes.h"
 #include QMK_KEYBOARD_H
 #include "features/achordion.h"
 
--- keyboards/beekeeb/piantor_pro/rules.mk
+++ keyboards/beekeeb/piantor_pro/rules.mk
@@ -3,6 +3,6 @@ SERIAL_DRIVER = vendor
 REPEAT_KEY_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = no
-KEY_OVERRIDE_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
 
 SRC += features/achordion.c
